239b0c3d1ba54b13b75146a95632bcb50f56bb91
emelyne1234 <e.uwimbabazai@alustudent.com>
2023-02-22 05:45:48 -0800

    set up

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,10 @@
 -- creating a database 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- creating a new user in local host
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
 -- setting a password
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 -- granting all privileges
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
