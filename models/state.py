f3c05ce71696175b0d90debfa78c724c3afb64b5
emelyne1234 <e.uwimbabazai@alustudent.com>
2023-02-23 03:35:03 -0800

    updates

--- models/state.py
+++ models/state.py
@@ -2,10 +2,15 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, DateTime, Integer
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__= 'states'
-    name = Column(String(128), nullable=False )
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
+
 
