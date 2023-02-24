f96baba7618229ded768232cff5699379127d528
emelyne1234 <e.uwimbabazai@alustudent.com>
2023-02-24 07:01:14 -0800

    storage update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
+""" database storage"""
 
+
+import os
+
+import sqlalchemy
+from sqlalchemy.orm import relationship, sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class DBStorage(): 
     """private class attributes"""
@@ -9,4 +22,64 @@ class DBStorage():
 
 
 def __init__(self):
-    self.__engine = 
+    user = os.getenv('HBNB_MYSQL_USER')
+    password = os.getenv('HBNB_MYSQL_PWD')
+    host = os.getenv('HBNB_MYSQL_HOST')
+    database = os.getenv('HBNB_MYSQL_DB')
+
+    self.__engine = sqlalchemy.create_engine(
+            'mysql+mysqldb://{}:{}@{}:3306/{}'
+            .format(user,
+                    password,
+                    host,
+                    database), pool_pre_ping=True)
+
+
+    if os.getenv('HBNB_ENV') == "test":
+            Base.metadata.drop_all(self.__engine)
+
+
+
+def all(self, cls=None):
+    """gets all objs"""
+    classes = {
+            'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
+        obj_dict = {}
+    if cls is not None and cls in classes:
+        class_objects = self.__session.query(classes[cls]).all()
+        for obj in class_objects:
+            key = obj.__class__.__name__ + "." + obj.id
+            obj_dict[key] = obj
+    if cls is None:
+        for cls in classes:
+            class_objects = self.__session.query(classes[cls]).all()
+            for obj in class_objects:
+                key = obj.__class__.__name__ + "." + obj.id
+                obj_dict[key] = obj
+
+    return obj_dict
+def new(self, obj):
+        """Adding the object"""
+    self.__session.add(obj)
+
+def save(self):
+        """saving commits"""
+    self.__session.commit()
+
+def delete(self, obj=None):
+        """Deleting from the current database"""
+    if obj is not None:
+        self.__session.delete(obj)
+
+def reload(self):
+    """creating all tables"""
+    Base.metadata.create_all(self.__engine)
+    session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+    session = scoped_session(session_factory)
+    self.__session = session()
+
+
--- models/place.py
+++ models/place.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+import models
+from models.review import Review
+from models.amenity import Amenity
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), nullable=False, ForeignKey='cities.id')
-    user_id = Column(String(60), nullable=False, ForeignKey='users.id')
+    city_id = Column(String(60), nullable=False, ForeignKey('cities.id', ondelete='CASCADE'))
+    user_id = Column(String(60), nullable=False, ForeignKey('users.id', ondelete='CASCADE')
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
@@ -21,3 +33,28 @@ class Place(BaseModel, Base):
     amenity_ids = []
     reviews = relationship('Review', backref='place')
 
+     if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def reviews(self):
+            """Returns the list of Review instances."""
+
+            reviews = list(models.storage.all(Review).values())
+
+            return list(
+                filter(lambda review: (review.place_id == self.id), reviews))
+
+        @property
+        def amenities(self):
+            """Returns the list of Amenity instances."""
+
+            amenities = list(models.storage.all(Amenity).values())
+
+            return list(
+                filter(lambda amenity: (amenity.place_id in self.amenity_ids),
+                       amenities))
+
+        @amenities.setter
+        def amenities(self, value=None):
+            """Adds ids in amenity_ids ."""
+            if type(value) == type(Amenity):
+                self.amenity_ids.append(value.id)
