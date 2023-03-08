c380a98897abbc17c1b78bb9f3bdbdec94aafe37
emelyne1234 <e.uwimbabazai@alustudent.com>
2023-03-08 08:29:29 -0800

    pack

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""fabric script that generates a .tgz"""
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
+    """returns the archive path"""
+    try:
+        date_str = datetime.now().strftime("%Y%m%d%H%M%S")
+        if not isdir("versions"):
+            local("mkdir versions")
+        archive_name = "versions/web_static_{}.tgz".format(date_str)
+        local("tar -cvzf {} web_static".format(archive_name))
+        return archive_name
+    except:
+        return None
