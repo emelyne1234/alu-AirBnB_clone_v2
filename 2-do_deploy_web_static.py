e37373f67fbbc177f4cd95230e73e2e392c5a045
emelyne1234 <e.uwimbabazai@alustudent.com>
2023-03-08 09:25:31 -0800

    deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""fabric script that distributes an archive to my web"""
+
+
+from os.path import exists
+from fabric.api import put, run, env
+
+
+env.hosts = ['54.225.38.130', '18.207.143.154']
+
+
+def do_deploy(archive_path):
+    """returns false if the file at the path archive doesn't exist"""
+    if not exists(archive_path):
+        return False
+
+    try:
+        filename = archive_path.split("/")[-1]
+        dirname = filename.split(".")[0]
+
+
+        dest_dir = "/data/web_static/releases/"
+
+        put(archive_path, '/tmp/')
+
+        run('mkdir -p {}{}/'.format(dest_dir, dirname))
+
+        run('tar -xzf /tmp/{} -C {}{}/'.format(filename, dest_dir, dirname))
+
+        run('rm /tmp/{}'.format(filename))
+
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(dest_dir, dirname))
+
+        run('rm -rf {}{}/web_static'.format(dest_dir, dirname))
+
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(dest_dir, dirname))
+
+        return True
+    except:
+        return False
+
