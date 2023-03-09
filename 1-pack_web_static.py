3f7f1015320053d7a87dd7f47c0b5f12102b2c84
emelyne1234 <105202770+emelyne1234@users.noreply.github.com>
2023-03-09 14:46:57 +0200

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,10 +9,10 @@ from os.path import isdir
 def do_pack():
     """returns the archive path"""
     try:
-        date_str = datetime.now().strftime("%Y%m%d%H%M%S")
+        date_format = datetime.now().strftime("%Y%m%d%H%M%S")
         if not isdir("versions"):
             local("mkdir versions")
-        archive_name = "versions/web_static_{}.tgz".format(date_str)
+        archive_name = "versions/web_static_{}.tgz".format(date_format)
         local("tar -cvzf {} web_static".format(archive_name))
         return archive_name
     except:
