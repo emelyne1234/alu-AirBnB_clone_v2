a7ca9f8cf24a73341d91c37225fca9c6fb81969e
emelyne1234 <e.uwimbabazi@alustudent.com>
2023-03-10 21:29:07 +0000

    pupppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
 """using puppet"""
 
+nginx_configuration = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
 #installing nginx if not there
 package { 'installing nginx':
   ensure   => 'present',
