41ff1f6db69625a7e43c3fc207ffa4a4a1094d68
emelyne1234 <105202770+emelyne1234@users.noreply.github.com>
2023-03-11 17:28:06 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime, Integer
-from models import storage
-from os import getenv
+
 
 Base = declarative_base()
 
