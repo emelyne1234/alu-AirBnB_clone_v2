79109cda0c9c23a6d1bdb7d6611e9d5cc191fcbb
emelyne1234 <105202770+emelyne1234@users.noreply.github.com>
2023-03-14 17:23:03 +0200

    Create 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a script that starts a flask application"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@ app.route('/', strict_slashes=False)
+def print_hello():
+    """hbnb"""
+    return "Hello HBNB!"
+
+
+@ app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """hbnb"""
+    return "HBNB!"
+
+
+@ app.route('/c/<string:name>', strict_slashes=False)
+def c_is_fun(name):
+    """C is fun"""
+    return "C {}".format(name)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
