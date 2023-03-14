264c744bf465a1fe8b418fbc3426e74eb3e40616
emelyne1234 <e.uwimbabazi@alustudent.com>
2023-03-14 12:04:30 +0000

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" a script that starts a flask application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@ app.route('/')
+def print_hello(name):
+    """hbnb"""
+    print(f'Hello {name}!')
+
+
+@ app.route('/hbnb')
+def print_hi(name):
+    """ printing hbnb"""
+    print(name)
+
+
+if __name__ == '__main__':
+    print_hi('HBNB')
+    app.run = (port := 5000, host := "0.0.0.0")
