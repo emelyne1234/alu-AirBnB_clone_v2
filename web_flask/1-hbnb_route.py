7554e24193eb8892730b01b8d69d539efe2be367
emelyne1234 <105202770+emelyne1234@users.noreply.github.com>
2023-03-14 16:23:27 +0200

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,15 +4,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-
-@ app.route('/')
+@ app.route('/', strict_slashes = False)
 def print_hello(name):
     """hbnb"""
     return (f'Hello {name}!')
 
 
-@ app.route('/hbnb')
+@ app.route('/hbnb', strict_slashes = False)
 def print_hi(name):
     """ printing hbnb"""
     return name
@@ -20,4 +20,4 @@ def print_hi(name):
 
 if __name__ == '__main__':
     print_hi('HBNB')
-    app.run = (port := 5000, host := "0.0.0.0")
+    app.run = (port := 5000, host := "0.0.0.0", debug=True)
